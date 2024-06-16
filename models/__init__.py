5e3bd09a277cb3d2716f223c4244e7a3990b3325
Edmond Skywalker <44839555+xiaoyao3302@users.noreply.github.com>
2024-06-16 21:18:04 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,4 @@
 from .point_mamba import PointMamba
 from .point_mamba import BasePointMamba
 from .point_mamba import PointMambaFormer
-from .point_mamba import PointMambaFormerPP
 from .build import build_model_from_cfg
